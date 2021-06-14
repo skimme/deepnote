54a81d964bf310b920932c580946556ade9469aa
skimme <wirsind@126.com>
2021-06-14 20:38:38 +0800

    added models

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from .efficient_net_v2 import efficientnetv2_l
+from .efficient_net_v2 import efficientnetv2_s
+from .efficient_net_v2 import efficientnetv2_m
+
+from .alex_net import alexnet
+
+from .res_net import resnet34
+from .res_net import resnet50
+from .res_net import resnet101
+from .res_net import resnext50_32x4d
+from .res_net import resnext101_32x8d
--- run_experiment.py
+++ run_experiment.py
@@ -3,12 +3,20 @@ from lit_model import LitModel
 import numpy as np
 import torch
 import pytorch_lightning as pl
-from model import AlexNet, resnet34
 import argparse
+import importlib
 
 
 np.random.seed(42)
 torch.manual_seed(42)
+
+
+def _import_class(module_and_class_name: str) -> type:
+    """Import class from a module, e.g. 'model.resnet34'"""
+    module_name, class_name = module_and_class_name.rsplit(".", 1)
+    module = importlib.import_module(module_name)
+    class_ = getattr(module, class_name)
+    return class_
 
 
 def _setup_parser():
@@ -23,8 +31,12 @@ def _setup_parser():
 
     # Basic arguments
     parser.add_argument("--data_class", type=str, default="MNIST")
-    parser.add_argument("--model_class", type=str, default="MLP")
+    parser.add_argument("--model_class", type=str, default="resnet34")
     parser.add_argument("--load_checkpoint", type=str, default=None)
+
+    # Get the data and model classes, so that we can add their specific arguments
+    # temp_args, _ = parser.parse_known_args()
+    # model_class = _import_class(f"models.{temp_args.model_class}")
 
     # Get data, model, and LitModel specific arguments
     data_group = parser.add_argument_group("Data Args")
@@ -40,18 +52,28 @@ def _setup_parser():
 def main():
     parser = _setup_parser()
     args = parser.parse_args()
+    model_class = _import_class(f"models.{args.model_class}")
+    model = model_class(num_classes=176)
 
     if args.load_checkpoint is not None:
         lit_model = LitModel.load_from_checkpoint(
-            args.load_checkpoint, model=resnet34(num_classes=176), args=args
+            args.load_checkpoint, model=model, args=args
         )
     else:
-        lit_model = LitModel(resnet34(num_classes=176), args=args)
+        lit_model = LitModel(model=model, args=args)
 
     data = MyDataModule(args)
     logger = pl.loggers.WandbLogger(project="kaggle_leaves")
     logger.watch(lit_model)
-    trainer = pl.Trainer.from_argparse_args(args, logger=logger)
+    logger.log_hyperparams(vars(args))
+    
+    early_stopping_callback = pl.callbacks.EarlyStopping(monitor="val_loss", mode="min", patience=10)
+    model_checkpoint_callback = pl.callbacks.ModelCheckpoint(
+        filename="{epoch:03d}-{val_loss:.3f}-{val_cer:.3f}", monitor="val_loss", mode="min"
+    )
+    callbacks = [early_stopping_callback, model_checkpoint_callback]
+
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks, logger=logger, weights_save_path="training/logs")
 
     # pylint: disable=no-member
     trainer.tune(
